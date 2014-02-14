9d9d2c5f7ba4520d103c047e59f662721592b8a3
Mike Rahnis <mike.rahnis@gmail.com>
2014-02-13 21:33:56 -0500

    Internal dataframe column names are lowercase.

--- orangery/ops/correction.py
+++ orangery/ops/correction.py
@@ -3,7 +3,7 @@ def get_offsets(df, coords):
 	Calulate the x,y,z offsets between a record in a dataframe and an array of x,y,z coordinates.
 	"""
 	if len(df) == 1:
-		offsets = [coords[0] - df.iloc[0]['X'], coords[1] - df.iloc[0]['Y'], coords[2] - df.iloc[0]['Z']]
+		offsets = [coords[0] - df.iloc[0]['x'], coords[1] - df.iloc[0]['y'], coords[2] - df.iloc[0]['z']]
 		return offsets
 	else:
 		print 'df can have only one record. df has ', len(df), ' records.'
@@ -12,7 +12,7 @@ def translate(df, offsets):
 	"""
 	Translate the x,y,z coordinates for records in a dataframe by an array of offsets.
 	"""
-	df['X'] = df['X'] + offsets[0]
-	df['Y'] = df['Y'] + offsets[1]
-	df['Z'] = df['Z'] + offsets[2]
+	df['x'] = df['x'] + offsets[0]
+	df['y'] = df['y'] + offsets[1]
+	df['z'] = df['z'] + offsets[2]
 	return df
