4c56dabda86f9cdd39834c427320b36ccc177b40
Michael Rahnis <michael.rahnis@fandm.edu>
2014-04-29 22:54:48 -0400

    simplify cut methods, move closing of line ends to own method

--- orangery/ops/geometry.py
+++ orangery/ops/geometry.py
@@ -121,20 +121,22 @@ def cut_by_distance(line, distance):
 	segments (LineString array) : array of cut line segments.
 
 	"""
-	if distance <= 0.0 or distance >= line.length:
+	if distance > 0.0 or distance < line.length:
+		coords = list(line.coords)
+		for i, c in enumerate(coords):
+			cd = line.project(Point(c))
+			if cd == distance:
+				segments = [
+					LineString(coords[:i+1]),
+					LineString(coords[i:])]
+			if cd > distance:
+				cp = line.interpolate(distance)
+				segments = [
+					LineString(coords[:i] + [(cp.x, cp.y)]),
+					LineString([(cp.x, cp.y)] + coords[i:])]
+	else:
 		segments = [LineString(line)]
-	coords = list(line.coords)
-	for i, p in enumerate(coords):
-		pd = line.project(Point(p))
-		if pd == distance:
-			segments = [
-				LineString(coords[:i+1]),
-				LineString(coords[i:])]
-		if pd > distance:
-			cp = line.interpolate(distance)
-			segments = [
-				LineString(coords[:i] + [(cp.x, cp.y)]),
-				LineString([(cp.x, cp.y)] + coords[i:])]
+
 	return segments
 
 def cut_by_point(line, pt):
@@ -151,20 +153,19 @@ def cut_by_point(line, pt):
 	segments (LineString array) : array of cut line segments.
 
 	"""
-	d = line.project(Point(pt))
-	if d <= 0.0 or d >= line.length:
-		segments = [LineString(line)]
-	coords = list(line.coords)
-	for i, c in enumerate(coords):
-		cd = line.project(Point(c))
-		if cd == d:
-			segments = [LineString(coords[:i]), LineString(coords[i:])]
-			break
-		elif cd > d:
-			segments = [LineString(coords[:i] + [(pt.x, pt.y)]), LineString([(pt.x, pt.y)] + coords[i:])]
-			break
+	distance = line.project(Point(pt))
+	if distance > 0.0 and distance < line.length:
+		coords = list(line.coords)
+		for i, c in enumerate(coords):
+			cd = line.project(Point(c))
+			if cd == distance:
+				segments = [LineString(coords[:i]), LineString(coords[i:])]
+				break
+			elif cd > distance:
+				segments = [LineString(coords[:i] + [(pt.x, pt.y)]), LineString([(pt.x, pt.y)] + coords[i:])]
+				break
 	else:
-		print('loop fell through without finding the point')
+		segments = [LineString(line)]
 
 	return segments
 
@@ -183,12 +184,8 @@ def cut_by_distances(line, intersections):
 
 	"""
 	for i in intersections:
-		d = line[-1].project(Point(i))
-		if len(list(line)) == 1:
-			line = cut_by_distance(line[-1], d)
-		elif len(list(line)) > 1:
-			cutline = cut_by_distance(line[-1], d)
-			line = line[:-1] + cutline
+		cutline = cut_by_distance(line[-1], i)
+		line = line[:-1] + cutline
 	segments = MultiLineString(line)
 	return segments
 
@@ -207,11 +204,8 @@ def cut_by_points(line, intersections):
 
 	"""
 	for i in intersections:
-		if len(list(line)) == 1:
-			line = cut_by_point(line[-1], i)
-		elif len(list(line)) > 1:
-			cutline = cut_by_point(line[-1], i)
-			line = line[:-1] + cutline
+		cutline = cut_by_point(line[-1], i)
+		line = line[:-1] + cutline
 	segments = MultiLineString(line)
 	return segments
 
@@ -291,7 +285,51 @@ def update(line, pt, idx):
 	newline = LineString(zip(xs, ys))
 	return newline
 
-def difference(line1, line2, close):
+def close(line1, line2):
+	try:
+		# prepend
+		if line1.coords[0][0] > line2.coords[0][0]:
+			# prepend to line1
+			bi = line2.intersection(
+				LineString([(line1.coords[0][0], line2.bounds[1]-1), (line2.coords[0][0], line2.bounds[3]+1)]))
+			if bi.y < line1.coords[0][1]:
+				prepend = Point([bi.x, bi.y-1])
+			else:
+				prepend = Point([bi.x, bi.y+1])
+			line1 = extend(line1, prepend, True)
+		else:
+			# prepend to line2
+			bi = line1.intersection(
+				LineString([(line2.coords[0][0], line1.bounds[1]-1), (line2.coords[0][0], line1.bounds[3]+1)]))
+			if bi.y < line2.coords[0][1]:
+				prepend = Point([bi.x, bi.y-1])
+			else:
+				prepend = Point([bi.x, bi.y+1])
+			line2 = extend(line2, prepend, True)
+		# append
+		if line1.coords[-1][0] < line2.coords[-1][0]:
+			# append to line1
+			ei = line2.intersection(LineString([(line1.coords[-1][0], line2.bounds[1]-1), (line1.coords[-1][0], line2.bounds[3]+1)]))
+			if ei.y < line1.coords[-1][1]:
+				append = Point([ei.x, ei.y-1])
+			else:
+				append = Point([ei.x, ei.y+1])
+			line1 = extend(line1, append, False)
+		else:
+			# append to line2
+			ei = line1.intersection(LineString([(line2.coords[-1][0], line1.bounds[1]-1), (line2.coords[-1][0], line1.bounds[3]+1)]))
+			if ei.y < line2.coords[-1][1]:
+				append = Point([ei.x, ei.y-1])
+			else:
+				append = Point([ei.x, ei.y+1])
+			line2 = extend(line2, append, False)
+	except:
+		print('Error: Unable to close line ends. You may need to flip one of your sections.')
+		raise
+
+	return line1, line2
+
+def difference(line1, line2, dangles=False):
 	"""
 	Create polygons from two LineString objects.
 
@@ -299,7 +337,7 @@ def difference(line1, line2, close):
 	----------
 	line1 (LineString) : a line representing the initial condition.
 	line2 (LineString) : a line representing the final condition.
-	close (bool) : option to close open line ends with vertical line segments.
+	dangles (bool) : option to close open line ends with vertical line segments.
 
 	Returns
 	-------
@@ -308,55 +346,16 @@ def difference(line1, line2, close):
 	signs (int array) : contains values of +1 or -1 to identify polygons as cut or fill.
 	
 	"""
-	if close==True:
-		try:
-			# prepend
-			if line1.coords[0][0] > line2.coords[0][0]:
-				# prepend to line1
-				bi = line2.intersection(
-					LineString([(line1.coords[0][0], line2.bounds[1]-1), (line1.coords[0][0], line2.bounds[3]+1)]))
-				if bi.y < line1.coords[0][1]:
-					prepend = Point([bi.x, bi.y-1])
-				else:
-					prepend = Point([bi.x, bi.y+1])
-				line1 = extend(line1, prepend, True)
-			else:
-				# prepend to line2
-				bi = line1.intersection(
-					LineString([(line2.coords[0][0], line1.bounds[1]-1), (line2.coords[0][0], line1.bounds[3]+1)]))
-				if bi.y < line2.coords[0][1]:
-					prepend = Point([bi.x, bi.y-1])
-				else:
-					prepend = Point([bi.x, bi.y+1])
-				line2 = extend(line2, prepend, True)
-			# append
-			if line1.coords[-1][0] < line2.coords[-1][0]:
-				# append to line1
-				ei = line2.intersection(LineString([(line1.coords[-1][0], line2.bounds[1]-1), (line1.coords[-1][0], line2.bounds[3]+1)]))
-				if ei.y < line1.coords[-1][1]:
-					append = Point([ei.x, ei.y-1])
-				else:
-					append = Point([ei.x, ei.y+1])
-				line1 = extend(line1, append, False)
-			else:
-				# append to line2
-				ei = line1.intersection(LineString([(line2.coords[-1][0], line1.bounds[1]-1), (line2.coords[-1][0], line1.bounds[3]+1)]))
-				if ei.y < line2.coords[-1][1]:
-					append = Point([ei.x, ei.y-1])
-				else:
-					append = Point([ei.x, ei.y+1])
-				line2 = extend(line2, append, False)
-		except:
-			print('Error: Unable to close line ends. You may need to flip one of your sections.')
-			raise
-			
+	if dangles==True:
+		line1, line2 = close(line1, line2)
+
 	intersections = line1.intersection(line2)
 
 	segs1 = cut_by_points([line1], intersections)
 	segs2 = cut_by_points([line2], intersections)
-
+	
 	polygons = polygonize([segs1, segs2])
-
+	
 	signs = sign(linemerge(segs1), linemerge(segs2))
 
 	# can't pass the polygonize generator to my class so convert the polygons into an array
