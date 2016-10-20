da1a8b10de13532202dd60607bf6c449004f2250
Mike Rahnis <mike.rahnis@gmail.com>
2016-10-19 21:22:42 -0400

    change to napoleon for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc',
 			  'sphinx.ext.autosummary',
-			  'numpydoc']
+			  'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
