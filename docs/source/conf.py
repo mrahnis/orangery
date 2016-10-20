9d716b83244d6d21f8957d6838a9dcb43c097b6b
Mike Rahnis <mike.rahnis@gmail.com>
2016-10-19 22:26:30 -0400

    add opusxml to mocks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ class Mock(object):
 		else:
 			return Mock()
 
-MOCK_MODULES = ['numpy','matplotlib','matplotlib.pyplot','pandas','shapely','shapely.geometry','shapely.ops']
+MOCK_MODULES = ['numpy','matplotlib','matplotlib.pyplot','pandas','shapely','shapely.geometry','shapely.ops', 'opusxml']
 for mod_name in MOCK_MODULES:
 	sys.modules[mod_name] = Mock()
 
