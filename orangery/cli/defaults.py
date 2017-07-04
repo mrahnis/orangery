c62afa49619efde8121f77122aca21b1f38777ba
Mike Rahnis <mike.rahnis@gmail.com>
2017-07-03 23:09:39 -0400

    add default survey codes

---
+++ orangery/cli/defaults.py
@@ -0,0 +1,11 @@
+codes = {
+	"codes": {
+		"marker": ["BASE","BM"],
+		"control": ["JST","JEND"],
+		"view": ["XS"],
+		"breakline": ["TRE","TW","WSE"]
+	},
+	"group": {
+		"column": "r"
+	}
+}
---
+++ orangery/cli/util.py
@@ -0,0 +1,15 @@
+def load_config(json_f):
+    """Load a json file containing configuration data
+
+    Parameters:
+        json_f: path to json file
+
+    Returns:
+        config (dict): dictionary of configuration data
+
+    """
+    import json
+
+    with open(json_f, 'r') as json_src:
+        config = json.load(json_src)
+    return config
