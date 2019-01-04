5f5d312bfe88b728c262bb469f611be83ea0e76a
Mike Rahnis <mike.rahnis@gmail.com>
2019-01-03 22:17:02 -0500

    Add an option to plot an overlay on top of the cutfill plot

--- orangery/cli/cutfill.py
+++ orangery/cli/cutfill.py
@@ -28,8 +28,9 @@ from orangery.tools.plotting import get_scale_factor
 @click.option('--close/--no-close', default=True, help="Close the line ends; --close is the default")
 @click.option('--reverse', type=click.Choice(['t0','t1','tx']), help="Reverse a line or lines of section (t0=initial, t1=final, tx=both)")
 @click.option('--exclude', nargs=2, type=click.Tuple([str, click.Choice(['t0','t1','tx'])]), multiple=True, metavar='<str choice>', help="Exclude a survey code from a line or lines of section (t0=initial, t1=final, tx=both)")
+@click.option('--overlay', nargs=1, type=click.Path(exists=True))
 @click.option('-v', '--verbose', is_flag=True, help="Enables verbose mode")
-def cutfill(file1, file2, fields, xs_name, codes_f, show, summary, units, labels, exaggeration, scale, close, reverse, exclude, verbose):
+def cutfill(file1, file2, fields, xs_name, codes_f, show, summary, units, labels, exaggeration, scale, close, reverse, exclude, overlay, verbose):
 	"""Displays a plot of a repeat survey with cut and fill.
 
 	\b
@@ -65,6 +66,9 @@ def cutfill(file1, file2, fields, xs_name, codes_f, show, summary, units, labels
 	s1 = o.Survey(file1, fields, codes, 0)
 	s2 = o.Survey(file2, fields, codes, 0)
 
+	if overlay:
+		s3 = o.Survey(overlay, fields, codes, 0)
+
 	exclude_t0 = []
 	exclude_t1 = []
 	for code in exclude:
@@ -77,6 +81,8 @@ def cutfill(file1, file2, fields, xs_name, codes_f, show, summary, units, labels
 	xs_pts1 = o.group(s1.data, s1.code_table, group=xs_name, exclude=exclude_t0)
 	xs_pts2 = o.group(s2.data, s2.code_table, group=xs_name, exclude=exclude_t1)
 
+	xs_pts_overlay = o.group(s3.data, s3.code_table, group=xs_name)
+
 	# get the endpoints of the group
 	p1, p2 = o.endpoints(xs_pts1, reverse=reverse in ('t0','tx'))
 
@@ -84,26 +90,35 @@ def cutfill(file1, file2, fields, xs_name, codes_f, show, summary, units, labels
 	xs1 = o.Section(xs_pts1, p1, p2, reverse=reverse in ('t0','tx'))
 	xs2 = o.Section(xs_pts2, p1, p2, reverse=reverse in ('t1','tx'))
 
+	xs_overlay = o.Section(xs_pts_overlay, p1, p2)
+
 	if labels:
 		label_t0 = labels[0]
 		label_t1 = labels[1]
+		label_overlay = labels[3]
 	elif 't' in fields:
 		label_t0 = (xs1.data.iloc[0]['t']).split('T')[0]
 		label_t1 = (xs2.data.iloc[0]['t']).split('T')[0]
+		label_overlay = (xs_overlay.data.iloc[0]['t']).split('T')[0]
 	else:
 		label_t0 = 't0'
 		label_t1 = 't1'
+		label_overlay = 'pre-restoration'
 
 	# calculate the change
 	chg = o.Change(xs1, xs2, close_ends=close)
 	if summary:
 		chg.summarize()
 
+	import matplotlib
+	font = {'family':'normal','weight':'normal','size':16}
+	matplotlib.rc('font', **font)
 	# plot the change between two cross-sections
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
 	ax.set_aspect(exaggeration)
 
+	xs_overlay.plot(ax=ax, marker='None', linestyle='-', linewidth=3, color='tab:red', label=label_overlay)
 	xs1.plot(ax=ax, marker='o', markersize=4, markerfacecolor='white', markeredgecolor='black', linestyle='-', color='gray', label=label_t0)
 	xs2.plot(ax=ax, marker='o', markersize=4, markerfacecolor='black', markeredgecolor='black', linestyle='-', color='black', label=label_t1)
 	chg.polygon_plot(ax=ax, fill_label='Fill', cut_label='Cut')
