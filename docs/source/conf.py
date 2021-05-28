f5b3d01a5c8ce27ad64163be21226b50e5b87fa5
Mike Rahnis <mike@topomatrix.com>
2021-05-28 01:17:31 -0400

    Add to mocks for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ class Mock(object):
 		else:
 			return Mock()
 
-MOCK_MODULES = ['numpy','matplotlib','matplotlib.pyplot','pandas','shapely','shapely.geometry','shapely.ops', 'opusxml']
+MOCK_MODULES = ['numpy','matplotlib','matplotlib.pyplot','matplotlib.lines','pandas','shapely','shapely.geometry','shapely.ops', 'opusxml']
 for mod_name in MOCK_MODULES:
 	sys.modules[mod_name] = Mock()
 
